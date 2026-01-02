dfc91fd0cfab82d839f4870f03678dd05aa781a7
Fnsf3 <83078056+Fnsf3@users.noreply.github.com>
2026-01-02 22:56:19 +0100

    yghujiokpjbhjnklk

--- assignment_5_activity_1_arithmeticUnit/arithmeticUnit.vhd
+++ assignment_5_activity_1_arithmeticUnit/arithmeticUnit.vhd
@@ -78,18 +78,18 @@ USE ieee.numeric_std.all;     --! SIGNED
 ENTITY arithmeticUnit is
 
    GENERIC (
-      N: INTEGER := 4  --! logic unit is designed for 4-bits
+      N  :		INTEGER 	:=	4  --! logic unit is designed for 4-bits
       
       --! Implement here CONSTANTS as GENERIC when required.
       
    );
    
    PORT (
-      A : IN  STD_LOGIC_VECTOR (N-1 DOWNTO 0); --! n-bit binary input
-      B : IN  STD_LOGIC_VECTOR (N-1 DOWNTO 0); --! n-bit binary input
-      P : IN  STD_LOGIC_VECTOR (3   DOWNTO 0); --! Flags input P(0)=Carry-bit
-      F : IN  STD_LOGIC_VECTOR (2   DOWNTO 0); --! 3-bit opcode
-      R : OUT STD_LOGIC_VECTOR (N   DOWNTO 0)  --! n+1-bit binary output
+      A : IN  STD_LOGIC_VECTOR (N-1 DOWNTO 0); --! OPERAND A	n-bit binary input
+      B : IN  STD_LOGIC_VECTOR (N-1 DOWNTO 0); --! Operand B	n-bit binary input
+      P : IN  STD_LOGIC_VECTOR (3   DOWNTO 0); --! FLAGS			Flags input P(0)=Carry-bit
+      F : IN  STD_LOGIC_VECTOR (2   DOWNTO 0); --! OPCODE 		3-bit opcode
+      R : OUT STD_LOGIC_VECTOR (N   DOWNTO 0)  --! RESULT 		n+1-bit binary output
    );
    
 END ENTITY arithmeticUnit;
@@ -97,9 +97,41 @@ END ENTITY arithmeticUnit;
 ARCHITECTURE implementation OF arithmeticUnit IS
    
    -- Implement here the SIGNALS to your descretion
-   
+	
+	SIGNAL u_A 	: UNSIGNED(N-1 DOWNTO 0); 
+	SIGNAL u_B	: UNSIGNED(N-1 DOWNTO 0);
+	SIGNAL u_R	: UNSIGNED(N   DOWNTO 0);
+	SIGNAL u_C  : UNSIGNED(N	DOWNTO 0); 	--1 bit used
+	
+	-- Operand A, B, R and C now unsigend
+	
+	SIGNAL bcdOpgeteld	:	UNSIGNED(N DOWNTO 0);
+	SIGNAL bcdAntwoord	:	UNSIGNED(N DOWNTO 0);
+	
+
 BEGIN
 
    -- Implement here your arithmetic unit.
-
-END ARCHITECTURE implementation;
+	
+	
+	bcdOpgeteld <= ('0' & u_A) + ('0' & u_B) + u_C;
+	bcdAntwoord <= bcdOpgeteld + 6 WHEN (bcdOpgeteld > 9) ELSE bcdOpgeteld;
+	
+	u_C(0) <= P(0);
+	u_C(N DOWNTO 1) <= (OTHERS => '0');
+	
+	
+	WITH F SELECT
+	u_R <= 	
+		(OTHERS => '0')							WHEN	"000",
+		('0' & u_A) + 1 							WHEN 	"001",
+		('0' & u_A) - 1							WHEN 	"010",
+		('0' & u_A) + ('0' & u_B)				WHEN	"011",
+		('0' & u_A) + ('0' & u_B) + u_C		WHEN 	"100",	
+		bcdAntwoord									WHEN 	"101",
+		('0' & u_A) - ('0' & u_B)				WHEN 	"110",
+		('0' & u_A) + ('0' & u_B) - u_C		WHEN 	"111",
+		(OTHERS => '0')	 						WHEN OTHERS;
+		
+	R <= STD_LOGIC_VECTOR (u_R(N DOWNTO 0));
+   END ARCHITECTURE implementation;
